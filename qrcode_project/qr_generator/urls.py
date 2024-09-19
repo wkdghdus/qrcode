8fbe761443902f8a0fab348783db8b9236312d2c
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-09-19 20:18:41 +0900

    day 2: worked on QR code generation with necessary information

---
+++ qrcode_project/qr_generator/urls.py
@@ -0,0 +1,12 @@
+#Imports the path function for URL routing.
+from django.urls import path
+from . import views
+
+
+# A list of URL patterns that maps a URL path to a view.
+# path('generate_qr/', views.generate_qr_code, name='generate_qr_code'): 
+# Maps the URL path generate_qr/ to the generate_qr_code view function. 
+# When this URL is accessed, Django will call generate_qr_code to handle the request.
+urlpatterns = [
+    path('generate_qr/', views.generate_qr_code, name='generate_qr_code'),
+]
